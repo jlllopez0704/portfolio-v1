96d2512fe77b98a06198c0ca773508172ab4c7d4
john lenn lopez <johnlennlopez@gmail.com>
2026-07-25 16:05:07 +0800

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/jlllopez-portfolio-v1/',
   plugins: [react()],
+  base: '/jlllopez-portfolio-v1/',
   server: {
     host: true,
     port: 3000,
